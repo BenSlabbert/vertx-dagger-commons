1b318ce42cd277a3a306ff4dfd8d178e66ec8958
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-05-25 13:42:03 +0200

    fixing flyway 10 migration

--- db-migration/src/main/java/github/benslabbert/vertxdaggercommons/dbmigration/FlywayProvider.java
+++ db-migration/src/main/java/github/benslabbert/vertxdaggercommons/dbmigration/FlywayProvider.java
@@ -1,34 +1,29 @@
 /* Licensed under Apache-2.0 2023. */
 package github.benslabbert.vertxdaggercommons.dbmigration;
 
-import java.util.Properties;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationVersion;
-import org.flywaydb.core.api.configuration.FluentConfiguration;
 
-public class FlywayProvider {
+public final class FlywayProvider {
+
   private FlywayProvider() {}
 
   public static Flyway get(
       String host, int port, String username, String password, String database) {
-    var flywayConfig = new FluentConfiguration().configuration(readFlywayProps());
 
     return Flyway.configure()
-        .configuration(flywayConfig)
+        .failOnMissingLocations(true)
+        .table("schema_version")
+        .locations("classpath:/migration")
+        .sqlMigrationPrefix("V")
+        .sqlMigrationSeparator("__")
+        .connectRetries(2)
+        .cleanDisabled(false)
+        .validateMigrationNaming(true)
         .dataSource(
             "jdbc:postgresql://%s:%d/%s".formatted(host, port, database), username, password)
         .baselineVersion(MigrationVersion.fromVersion("0"))
         .target(MigrationVersion.LATEST)
         .load();
-  }
-
-  private static Properties readFlywayProps() {
-    try (var stream = FlywayProvider.class.getResourceAsStream("/flyway/flyway.conf")) {
-      var props = new Properties();
-      props.load(stream);
-      return props;
-    } catch (Exception e) {
-      throw new MigrationException(e);
-    }
   }
 }

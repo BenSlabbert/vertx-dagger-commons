4146a75162e17e5d960c78858ca23fc59e9f6ffb
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-06-09 18:53:06 +0200

    deprecate FreePortUtility

--- commons/src/test/java/github/benslabbert/vertxdaggercommons/FreePortUtility.java
+++ commons/src/test/java/github/benslabbert/vertxdaggercommons/FreePortUtility.java
@@ -8,6 +8,10 @@ public final class FreePortUtility {
 
   private FreePortUtility() {}
 
+  /**
+   * @deprecated use 0 to get a random free port
+   */
+  @Deprecated(forRemoval = true, since = "1.19.0")
   public static int getPort() {
     for (int i = 0; i < 1_000; i++) {
       try (var serverSocket = new ServerSocket(0)) {

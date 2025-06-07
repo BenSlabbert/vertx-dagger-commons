9af2d7446582d9ab86f1188d7acc2ef58391eba5
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-06-07 17:30:39 +0200

    overload

--- db-migration/src/main/java/github/benslabbert/vertxdaggercommons/dbmigration/FlywayProvider.java
+++ db-migration/src/main/java/github/benslabbert/vertxdaggercommons/dbmigration/FlywayProvider.java
@@ -1,16 +1,26 @@
 /* Licensed under Apache-2.0 2023. */
 package github.benslabbert.vertxdaggercommons.dbmigration;
 
+import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationVersion;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
 
 public final class FlywayProvider {
 
   private FlywayProvider() {}
 
+  public static Flyway get(DataSource dataSource) {
+    return baseConfiguration().dataSource(dataSource).load();
+  }
+
   public static Flyway get(
       String host, int port, String username, String password, String database) {
+    String url = "jdbc:postgresql://%s:%d/%s".formatted(host, port, database);
+    return baseConfiguration().dataSource(url, username, password).load();
+  }
 
+  private static FluentConfiguration baseConfiguration() {
     return Flyway.configure()
         .failOnMissingLocations(true)
         .table("schema_version")
@@ -20,10 +30,7 @@ public final class FlywayProvider {
         .connectRetries(2)
         .cleanDisabled(false)
         .validateMigrationNaming(true)
-        .dataSource(
-            "jdbc:postgresql://%s:%d/%s".formatted(host, port, database), username, password)
         .baselineVersion(MigrationVersion.fromVersion("0"))
-        .target(MigrationVersion.LATEST)
-        .load();
+        .target(MigrationVersion.LATEST);
   }
 }

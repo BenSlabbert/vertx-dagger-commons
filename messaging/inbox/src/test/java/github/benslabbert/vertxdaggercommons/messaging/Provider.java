8d41e80dce871a135688d1f3dc03dd5c94b18b9b
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-07-19 15:01:00 +0200

    adding inbox

--- messaging/inbox/src/test/java/github/benslabbert/vertxdaggercommons/messaging/Provider.java
+++ messaging/inbox/src/test/java/github/benslabbert/vertxdaggercommons/messaging/Provider.java
@@ -1,20 +1,32 @@
 /* Licensed under Apache-2.0 2025. */
 package github.benslabbert.vertxdaggercommons.messaging;
 
+import dagger.Binds;
 import dagger.BindsInstance;
 import dagger.Component;
+import dagger.Module;
+import github.benslabbert.txmanager.TransactionManager;
 import github.benslabbert.vertxdaggercommons.transaction.blocking.jdbc.JdbcQueryRunnerFactory;
 import github.benslabbert.vertxdaggercommons.transaction.blocking.jdbc.JdbcTransactionManager;
 import io.vertx.core.Vertx;
 import jakarta.inject.Singleton;
 
 @Singleton
-@Component
+@Component(modules = {Provider.ModuleBindings.class})
 public interface Provider {
 
   MyInbox myInbox();
 
   JdbcQueryRunnerFactory jdbcQueryRunnerFactory();
+
+  TransactionManager transactionManager();
+
+  @Module
+  interface ModuleBindings {
+
+    @Binds
+    TransactionManager bindTransactionManager(JdbcTransactionManager jdbcTransactionManager);
+  }
 
   @Component.Builder
   interface Builder {
